3151c3a722cd549b4b8c33bb217a1f21962d52e1
Ozzie Harb <ozharb@gmail.com>
2021-02-21 10:19:50 -0800

    edit

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@
     
 //   }
   export default {
-    REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8080/api'
+    REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api'
     }
