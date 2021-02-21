e3a37efe31056cb4fa08e14a1572a5b1569b1a2a
Ozzie Harb <ozharb@gmail.com>
2021-02-21 11:57:01 -0800

    config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,3 @@
-// //eslint-disable-next-line import/no-anonymous-default-export
-// export default {
-//     API_ENDPOINT: 'https://petful-server-oz.herokuapp.com/api',
-//     //TestingEndpoint://
-//     //API_ENDPOINT: 'http://localhost:8000/api',
-//     TOKEN_KEY: '',
-    
-    
-//   }
-  export default {
+export default {
     REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api'
     }
